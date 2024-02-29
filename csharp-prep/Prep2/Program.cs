dfb7dc54eca094a0f4cb3a072c340b7c8fb631ea
mcm23016 <mcm23016@byui.edu>
2024-02-28 20:47:19 -0700

    Wrote the first core challenge for the assignment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        // Asks user for grade percentage 
+        Console.Write("Enter your grade percentage: ");
+        string userInput = Console.ReadLine();
+
+        // Converts grade percentage string to an int
+        int gradePercentage = int.Parse(userInput);
+
+        // Decides the letter grade to give to the student
+        if (gradePercentage >= 90){
+            Console.WriteLine("Your grade is an A");
+        } else if (gradePercentage >= 80){
+            Console.WriteLine("Your grade is an B");
+        } else if (gradePercentage >= 70){
+            Console.WriteLine("Your grade is an C");
+        } else if (gradePercentage >= 60){
+            Console.WriteLine("Your grade is an D");
+        } else {
+            Console.WriteLine("Your grade is an F");
+        }
     }
 }
