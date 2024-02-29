5d95c4fd47b478c39327b6fcf72cc591e2fb62ec
mcm23016 <mcm23016@byui.edu>
2024-02-28 20:36:27 -0700

    Made the first two questions Console.Write instead of Console.WriteLine and added a space after the question marks.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,9 +5,9 @@ class Program
     static void Main(string[] args)
     {
         // Gets the users First and Last names
-        Console.WriteLine("What is your first name?");
+        Console.Write("What is your first name? ");
         string first_name = Console.ReadLine();
-        Console.WriteLine("What is your last name?");
+        Console.Write("What is your last name? ");
         string last_name = Console.ReadLine();
 
         // Prints the users First and Last names
