2f8a324587bc8d6a9cce0cccbbca9acfa706c6d2
mcm23016 <mcm23016@byui.edu>
2024-02-28 21:20:50 -0700

    Completed the second core requirment for the assignment.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
-    }
+        // Gets a magic number
+        Console.Write("What is the magic number? ");
+        int magicNumber = int.Parse(Console.ReadLine());
+
+        // Loops through program until user guesses correct number
+        bool flag = true;
+        while (flag) {
+
+            // Gets a guess
+            Console.Write("What is your guess? ");
+            int guess = int.Parse(Console.ReadLine());
+
+            // Compares magic number to guess
+            if (guess == magicNumber) // The guess is right 
+            {
+                Console.WriteLine("You guessed it!");
+                flag = false;
+            } 
+            else if (guess > magicNumber) // The guess is too high
+            {
+                Console.WriteLine("Lower");
+            }
+            else // The guess must be too lower
+            {
+                Console.WriteLine("Higher");
+            }
+        }
+    }   
 }
