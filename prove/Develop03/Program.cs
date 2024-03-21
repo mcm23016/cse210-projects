4473354c646ccb40c08e2a9ed73024eed2fdc8c1
mcm23016 <mcm23016@byui.edu>
2024-03-21 10:57:25 -0600

    I haven't committed in a little bit.

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        void ImGetScripture()
+        {
+            // Help the user know what they are entering
+            Console.WriteLine("Please enter the information of the scripture you want to memorize:");
+            
+            // Gets scripture refrence
+            Console.Write("Refrence: ");
+            string imRefrence = Console.ReadLine();
+
+            // Gets scripture passage
+            Console.Write("Passage: ");
+            string imPassage = Console.ReadLine(); 
+
+            // Creates Scripture object
+            Scripture s = new Scripture(imRefrence, imPassage);
+        }
+
+
+
+
+
+
+        // Code to run
+        ImGetScripture();
+        ImHideWords();
     }
 }
--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -1,9 +1,53 @@
 using System;
+using System.Collections;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        static void ImMenu()
+        {
+            // Variable for user choice
+            string imUserChoice = "";
+
+            while(imUserChoice != "4")
+            {
+                // Displays menu
+                Console.WriteLine("Menu Options:");
+                Console.WriteLine("1. Start Breathing Activity");
+                Console.WriteLine("2. Start Listing Activity");
+                Console.WriteLine("3. Start Reflecting Activity");
+                Console.WriteLine("4. Quit");
+                
+                // Gets user choice
+                Console.Write("Select a choice from the menu: ");
+                imUserChoice = Console.ReadLine();
+
+                // Decides what to do
+                switch(imUserChoice)
+                {
+                    case "1":
+                        // Breathing Activity
+                        break;
+                    case "2":
+                        // Listing Activity
+                        break;
+                    case "3":
+                        // Reflecting Activity
+                        break;
+                    case "4":
+                        // Quit
+                        break;
+                    default:
+                        //Catches all else
+                        Console.WriteLine("Error: Enter a number 1-4");
+                        break;
+                }
+                
+            }
+        }
+
+
+        ImMenu();
     }
 }
